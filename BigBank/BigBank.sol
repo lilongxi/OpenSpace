bacb45bf1262f17564724d78998c01e4af0f95e2
leelongxi <leelongxi@foxmail.com>
2024-10-13 17:38:42 +0800

    chore: forge init

---
+++ BigBank/BigBank.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+interface IBank {
+    function  desposit() external payable;
+    function withdraw(uint amount) external;
+    function getBalance() external view returns (uint);
+}
