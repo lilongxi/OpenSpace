39e7aa4ef25843fa7d3b715daa0ea3aa3719aa97
leelongxi <leelongxi@foxmail.com>
2024-10-13 20:01:14 +0800

    feat: OpenSpace 完成 Admin  编写

---
+++ BigBank/Admin.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import "lib/OpenSpace/BankContract/Bank.sol";
+
+contract Admin {
+    address public owner;
+
+    error NotOwner();
+    error NoEnoughBalance();
+    error isZeroAddress();
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function adminWithdraw(IBank bank) external onlyOwner {
+        uint balance = address(bank).balance;
+        if (balance <= 0) revert NoEnoughBalance();
+        bank.withdraw(balance);
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner zeroAddress(newOwner) {
+        owner = newOwner;
+    }
+
+     modifier onlyOwner() {
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
+
+    modifier zeroAddress(address newOwner) {
+        if (newOwner == address(0)) revert isZeroAddress();
+        _;
+    }
+
+}
--- BigBank/BigBank.sol
+++ BigBank/BigBank.sol
@@ -1,9 +1,28 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.26;
 
-interface IBank {
-    function  desposit() external payable;
-    function withdraw(uint amount) external;
-    function getBalance() external view returns (uint);
-}
+import "lib/OpenSpace/BankContract/Bank.sol";
 
+contract BigBank is Bank {
+    uint public constant MIN_DEPOSIT = 0.001 ether;
+
+    error NotGreaterThanMinDeposit();
+    error isZeroAddress();
+
+    modifier minDeposit() {
+        if (msg.value < MIN_DEPOSIT) revert NotGreaterThanMinDeposit();
+        _;
+    }
+
+    // 重写存款方法
+    function deposit() public payable override minDeposit {
+        super.deposit();  // 调用父合约的存款方法
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        if (newOwner == address(0)) revert isZeroAddress();
+        owner = newOwner;
+    }
+    
+
+}
