e3e23b5e4f82a0cb75e7fde43ff08f214ca56ce0
leelongxi <leelongxi@foxmail.com>
2024-10-18 12:26:24 +0800

    feat:

--- test/Bank.t.sol
+++ test/Bank.t.sol
@@ -8,33 +8,34 @@ contract FristBankTest is Test {
 
     Bank bank;
 
+    event Deposit(address indexed user, uint amount);
+
     function setUp() public {
         bank = new Bank();
     }
-
-    event Deposit(address indexed user, uint amount);
 
     function testDepositETH() public {
 
     address user = address(0x123);
     uint initialDeposit = 1 ether;
 
-    // 模拟用户地址调用
-    vm.prank(user);
+    vm.deal(user, initialDeposit);
 
     // 预期事件的输出：设置预期捕获的事件
     vm.expectEmit(true, true, false, true);
     emit Deposit(user, initialDeposit);
 
-    // 检查存款前余额
+      // 检查存款前余额
     assertEq(bank.balanceOf(user), 0, "Initial balance should be zero");
 
-    // 执行存款逻辑
+    // 模拟用户地址调用
+    vm.prank(user);
+
+     // 执行存款逻辑
     bank.depositETH{value: initialDeposit}();
 
     // 检查存款后的余额是否正确更新
     assertEq(bank.balanceOf(user), initialDeposit, "Balance should be updated after deposit");
-        
 
     }
 
@@ -44,7 +45,7 @@ contract FristBankTest is Test {
         vm.prank(user);
 
         vm.expectRevert("Deposit amount must be greater than 0");
-
+        
         bank.depositETH{value: 0}();
     }
 }
