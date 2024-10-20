c6a0aed3921be3f12f4aa2a1be062922128c31b2
leelongxi <leelongxi@foxmail.com>
2024-10-20 20:37:39 +0800

    feat: 完成 Viem.sh 合约调用

---
+++ test/NFTMarketFuzzTest.t.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import { NFTMarket, NFTMarketEvent } from "src/chapter-006/NFTMarket/NFTMarket.sol";
+import { BaseERC20 } from "src/BaseERC20.sol";
+import { BaseERC721 } from "src/chapter-006/MyERC721.sol";
+
+contract NFTMarketFuzzTest is Test {
+    
+}
