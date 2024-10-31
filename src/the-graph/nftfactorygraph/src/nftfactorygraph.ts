c8473a29ccbe81396370b04c5fee8aa816a8c38a
leelongxi <leelongxi@foxmail.com>
2024-10-31 22:49:23 +0800

    feat: Initial commit

--- src/the-graph/nftfactorygraph/src/nftfactorygraph.ts
+++ src/the-graph/nftfactorygraph/src/nftfactorygraph.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: leelongxi leelongxi@foxmail.com
+ * @Date: 2024-10-31 18:12:30
+ * @LastEditors: leelongxi leelongxi@foxmail.com
+ * @LastEditTime: 2024-10-31 21:07:25
+ * @FilePath: /OpenSpace/src/the-graph/nftfactorygraph/src/nftfactorygraph.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import {
   NFTCreated as NFTCreatedEvent,
   NFTRegesitered as NFTRegesiteredEvent,
@@ -6,7 +14,8 @@ import {
 import {
   NFTCreated,
   NFTRegesitered,
-  OwnershipTransferred
+  OwnershipTransferred,
+  TokenInfo
 } from "../generated/schema"
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
@@ -49,4 +58,27 @@ export function handleOwnershipTransferred(
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+}
+
+export function handleNFTCreatedByNFTCA(event: NFTCreatedEvent): void {
+
+  const nftAddress = event.params.nftCA
+  // entity.nftCA = event.params.nftCA
+
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
+
+  // const entity = new TokenInfo(event.transaction.hash.concatI32(event.logIndex.toI32()).toString())
+
+  // entity.ca = nftAddress
+  // entity.tokenId = event.params.name
+  // entity.tokenURL = event.params.tokenURL
+  // entity.name = tokenInfo.name
+  // entity.owner = tokenInfo.owner
+  // entity.blockNumber = event.block.number
+  // entity.blockTimestamp = event.block.timestamp
+  // entity.transactionHash = event.transaction.hash
+  // entity.save()
+
 }
