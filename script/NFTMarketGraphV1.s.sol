e5845b14afd7304825e7b3dc0ec23077c0a27cda
leelongxi <leelongxi@foxmail.com>
2024-10-31 10:05:40 +0800

    feat: Initial commit

---
+++ script/NFTMarketGraphV1.s.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import { NFTMarketGraphV1 } from "../src/the-graph/NFTMarket.sol";
+import { OSGraphNFT } from "../src/the-graph/MYNFTGraph.sol";
+import { MyGraphToken } from "../src/the-graph/MyGraphToken.sol";
+
+contract NFTMarketGraphV1Script is Script {
+    NFTMarketGraphV1 public nftMarket;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        nftMarket = new NFTMarketGraphV1();
+
+        vm.stopBroadcast();
+    }
+}
+
+contract OSGraphNFTScript is Script {
+    OSGraphNFT public osGraphNFT;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        osGraphNFT = new OSGraphNFT();
+
+        vm.stopBroadcast();
+    }
+}
+
+contract MyGraphTokenScript is Script {
+    MyGraphToken public myGraphToken;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        myGraphToken = new MyGraphToken();
+
+        vm.stopBroadcast();
+    }
+}
--- src/chapter-012-permit2/TokenbankPermit2.sol
+++ src/chapter-012-permit2/TokenbankPermit2.sol
@@ -1,43 +1,43 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 
-pragma solidity >=0.7.0 <0.9.0;
+// pragma solidity >=0.7.0 <0.9.0;
 
-import "oz_v5/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "permit2/src/Permit2.sol"; // Permit2合约的接口或具体实现
+// import "oz_v5/contracts/token/ERC20/extensions/ERC20Permit.sol";
+// import "permit2/src/Permit2.sol"; // Permit2合约的接口或具体实现
 
-contract TokenBankPermit2 {
+// contract TokenBankPermit2 {
 
-    ERC20Permit public token;
-    Permit2 public permit2;
+//     ERC20Permit public token;
+//     Permit2 public permit2;
 
-    mapping (address => uint) public balances;
+//     mapping (address => uint) public balances;
 
-    event Desposit(address indexed user, uint amount);
-    event DespositWithPermit2(address indexed user, uint amount);
+//     event Desposit(address indexed user, uint amount);
+//     event DespositWithPermit2(address indexed user, uint amount);
 
-    constructor(address _token, address _permit2) {
-        token = ERC20Permit(_token);
-        permit2 = Permit2(_permit2);
-    }
+//     constructor(address _token, address _permit2) {
+//         token = ERC20Permit(_token);
+//         permit2 = Permit2(_permit2);
+//     }
 
-    function  depositWithPermit2(
-        address owner,
-        uint amount,
-        uint deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    )  external {
-          // 使用 Permit2 进行授权
-        permit2.permit(owner, address(this), amount, deadline, v, r, s);
-        // 执行存款逻辑
-        token.transferFrom(owner, address(this), amount);
-        balances[owner] += amount;
-        emit DespositWithPermit2(owner, amount);
-    }
+//     function  depositWithPermit2(
+//         address owner,
+//         uint amount,
+//         uint deadline,
+//         uint8 v,
+//         bytes32 r,
+//         bytes32 s
+//     )  external {
+//           // 使用 Permit2 进行授权
+//         permit2.permit(owner, address(this), amount, deadline, v, r, s);
+//         // 执行存款逻辑
+//         token.transferFrom(owner, address(this), amount);
+//         balances[owner] += amount;
+//         emit DespositWithPermit2(owner, amount);
+//     }
 
-    function balanceOf(address account) external view returns (uint) {
-        return balances[account];
-    }
+//     function balanceOf(address account) external view returns (uint) {
+//         return balances[account];
+//     }
 
-}
+// }
