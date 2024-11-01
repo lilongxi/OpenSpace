4a94e71691ac5bdcb7542d561d53e8730e572672
leelongxi <leelongxi@foxmail.com>
2024-11-01 13:24:00 +0800

    feat: Initial commit

--- src/the-graph/nftfactorygraph/src/nftfactorygraph.ts
+++ src/the-graph/nftfactorygraph/src/nftfactorygraph.ts
@@ -2,7 +2,7 @@
  * @Author: leelongxi leelongxi@foxmail.com
  * @Date: 2024-10-31 18:12:30
  * @LastEditors: leelongxi leelongxi@foxmail.com
- * @LastEditTime: 2024-11-01 13:02:01
+ * @LastEditTime: 2024-11-01 13:23:23
  * @FilePath: /OpenSpace/src/the-graph/nftfactorygraph/src/nftfactorygraph.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,19 +20,22 @@ import {
 import { NFTContract } from "../generated/templates";
 
 export function handleNFTCreated(event: NFTCreatedEvent): void {
-  // let entity = new NFTCreated(
-  //   event.transaction.hash.concatI32(event.logIndex.toI32())
-  // )
-  // entity.nftCA = event.params.nftCA
 
-  // entity.blockNumber = event.block.number
-  // entity.blockTimestamp = event.block.timestamp
-  // entity.transactionHash = event.transaction.hash
-
-  // entity.save()
 
   // 创建数据源
   NFTContract.create(event.params.nftCA);
+
+  // 设置数据源数据
+  let entity = new NFTCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.nftCA = event.params.nftCA
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
 
 }
 
