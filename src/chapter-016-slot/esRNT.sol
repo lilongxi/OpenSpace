92d4b0400bb27928ba6ee3fc5457a47f1fd3dfbe
leelongxi <leelongxi@foxmail.com>
2024-11-06 17:33:17 +0800

    feat: Initial commit

--- src/chapter-016-slot/esRNT.sol
+++ src/chapter-016-slot/esRNT.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.26;
 contract esRNT {
 
     struct LockInfo {
-        address user;
-        uint64 startTime;
-        uint256 amount;
+        address user; // 20 bytes
+        uint64 startTime; // 8 bytes
+        uint256 amount;  // 32 bytes
     }
 
     LockInfo[] private _locks;
 
     constructor() {
         for (uint i = 0; i < 11; i++) {
+            /**
+             * uint160(i+1) 强转 20 bytes
+             */
             _locks.push(LockInfo(address(uint160(i+1)), uint64(block.timestamp*2-i), 1e18*(i+1)));
         }
     }
---
+++ test/MyWalletOwnerTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import { MyWalletV1 } from "src/chapter-016-slot/MyWalletV1.sol";
+
+contract MyWalletOwnerTest is Test {
+
+    MyWalletV1 myWallet;
+
+    address owner = address(0x123);
+    address newOwner = address(0x456);
+    address nonOwner = address(0x789);
+    string walletName = "My Wallet";
+
+    function setUp() public {
+         myWallet = new MyWalletV1(walletName);
+         // 验证部署时的初始 owner 是否为 address(0x123)
+        assertEq(myWallet.owner(), owner);
+    }
+
+    function testTransferOwnership() public {
+         vm.startPrank(owner);  // 模拟为 owner 地址进行操作
+         myWallet.transferOwnership(newOwner);
+         vm.stopPrank();
+         assertEq(myWallet.owner(), newOwner);
+    }
+
+    function testTransferOwnershipNonOwner() public {
+        // 测试非 owner 调用 transferOwnership 的情况
+        vm.startPrank(nonOwner);  // 模拟为非 owner 地址进行操作
+        vm.expectRevert("Not authorized");  // 期望 revert
+        myWallet.transferOwnership(newOwner);
+        vm.stopPrank();
+    }
+
+    function testTransferOwnershipToZeroAddress() public {
+        // 测试转移所有权到零地址
+        vm.startPrank(owner);
+        vm.expectRevert("New owner is the 0 address");  // 期望 revert
+        myWallet.transferOwnership(address(0));
+        vm.stopPrank();
+    }
+}   
